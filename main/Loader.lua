216eed8165a88b932b2b43e4eda19be49147ebe5
x9SunZ <116784765+x9SunZ@users.noreply.github.com>
2024-03-08 21:23:21 +0700

    Update Loader.lua

--- main/Loader.lua
+++ main/Loader.lua
@@ -6,5 +6,5 @@ else
 end
 
 if MuderMystery2 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x9SunZ/Serity-Trash/main/Muder%20Mystery%202"))
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x9SunZ/Serity-Trash/main/Muder%20Mystery%202"))()
 end
